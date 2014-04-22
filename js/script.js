4831367233576eb5faf82b1af9a9545818687728
john <john@buscher-laptop.(none)>
2014-04-22 12:39:27 -0400

    Updated script to take input for city

--- js/script.js
+++ js/script.js
@@ -17,22 +17,28 @@ document.addEventListener("DOMComponentsLoaded", function(){
 		
 });
 $(document).ready(function(){
-	$.simpleWeather({
-		location: 'Fairfax, VA',
-		woeird: '',
-		unit: 'f',
-		success: function(weather){
-			html = '<h2>'+weather.city+', '+weather.region+'</h2>';
-			html+= '<p>'+weather.temp+'&deg; '+weather.units.temp+'<br/><span>'+weather.currently+'</span></p>';
-			html+= '<a href="'+weather.link+'">View Forecast</a>';
+	$("#update").on('click', function(){
+		$.simpleWeather({
+			location: $("#area").val(),
+			woeird: '',
+			unit: 'f',
+			success: function(weather){
+				html = '<h2>'+weather.city+', '+weather.region+'</h2>';
+				html+= '<p>'+weather.temp+'&deg; '+weather.units.temp+'<br/><span>'+weather.currently+'</span></p>';
+				html+= '<a href="'+weather.link+'">View Forecast</a>';
 
-			$("#weather").html(html);
-		},
-		error: function(error) {
-			$("#weather").html("<p>"+error+"</p>");
-		}
+				$("#weather").html(html);
+			},
+			error: function(error) {
+				$("#weather").html("<p>"+error+"</p>");
+			}
+		});
 	});
 });
+
+function updateWeather(){
+	
+}
 /*
 
 JSON weather stuff
